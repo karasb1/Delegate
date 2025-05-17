a3823b051b5be28c09305283827c658b3175ac63
karasb1 <bohdankaras456@gmail.com>
2025-05-17 17:08:34 +0300

    feat: add methods to calculate area of triangle and rectangle, and print current time, date, and day of the week

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Interfaces;
-
-class Program
+﻿namespace Delegate
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!"); 
+        static void Main(string[] args)
+        {
+            double baseLength = 5;
+            double height = 10;
+            Action printCurrentTime = PrintCurrentTime;
+            Action printCurrentDate = PrintCurrentDate;
+            Action printDayOfWeek = PrintDayOfWeek;
+            Func<double, double, double> triangleArea = TriangleArea;
+            Func<double, double, double> rectangleArea = RectangleArea;
+            printCurrentTime.Invoke();
+            printCurrentDate.Invoke();
+            printDayOfWeek.Invoke();
+            Console.WriteLine(triangleArea(baseLength, height));
+            Console.WriteLine(rectangleArea(baseLength, height));
+        }
+        static void PrintCurrentTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+        }
+        static void PrintCurrentDate()
+        {
+            Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy"));
+        }
+        static void PrintDayOfWeek()
+        {
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+        static double TriangleArea(double baseLength, double height)
+        {
+            return (baseLength * height) / 2;
+        }
+        static double RectangleArea(double baseLength, double height)
+        {
+            return baseLength * height;
+        }
     }
 }
