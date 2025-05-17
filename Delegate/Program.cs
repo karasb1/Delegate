841aa20f97292620b3550ac13d1ce55a2dbf313e
karasb1 <bohdankaras456@gmail.com>
2025-05-17 14:09:23 +0300

    feat: implement delegate methods for basic arithmetic operations

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Delegate;
-
-class Program
+﻿namespace Delegate
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!"); 
+        public delegate int DelCalculate(int a, int b);
+
+        public static void Main(string[] args)
+        {
+            DelCalculate del = new DelCalculate(Add);
+            DelCalculate del2 = new DelCalculate(Subtract);
+            DelCalculate del3 = new DelCalculate(Multiply);
+            
+            Console.WriteLine("Addition: " + del(10, 20));
+            Console.WriteLine("Subtraction: " + del2(20, 10));
+            Console.WriteLine("Multiplication: " + del3(10, 20));
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
