c0f5617bdf0254386f7596cca2c558f5d940ec6f
karasb1 <bohdankaras456@gmail.com>
2025-05-17 14:05:37 +0300

    feat: implement delegate methods for message display in Program.cs

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Delegate;
-
-class Program
+﻿namespace Delegate
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!"); 
+        delegate void ShowMessage(string message);
+
+        static void Main(string[] args)
+        {
+            ShowMessage showMessage = Show;
+            showMessage += Show2;
+            showMessage += Show3;
+            showMessage("Hello, World!");
+        }
+        static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Show2(string message)
+        {
+            Console.WriteLine(message.ToUpper());
+        }
+        static void Show3(string message)
+        {
+            Console.WriteLine(message.ToLower());
+        }
     }
 }
