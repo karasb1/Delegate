34968b3262547582e2e66a4d8941f89fdb7ac523
karasb1 <bohdankaras456@gmail.com>
2025-05-17 16:38:06 +0300

    feat: add array operations for even, odd, prime, and Fibonacci numbers

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,9 +1,57 @@
-﻿namespace Interfaces;
-
-class Program
+﻿namespace Delegate
 {
-    static void Main(string[] args)
+    delegate int[] ArrayOperation(int[] arr);
+    class Program
     {
-        Console.WriteLine("Hello, World!"); 
+        static void Main(string[] args)
+        {
+            int [] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ArrayOperation GetEvenNumbers = EvenNumbers;
+            ArrayOperation GetOddNumbers = OddNumbers;
+            ArrayOperation GetPrimeNumbers = PrimeNumbers;
+            ArrayOperation GetFibonacciNumbers = FibonacciNumbers;
+            
+            Console.WriteLine("Even Numbers: " + string.Join(", ", GetEvenNumbers(numbers)));
+            Console.WriteLine("Odd Numbers: " + string.Join(", ", GetOddNumbers(numbers)));
+            Console.WriteLine("Prime Numbers: " + string.Join(", ", GetPrimeNumbers(numbers)));
+            Console.WriteLine("Fibonacci Numbers: " + string.Join(", ", GetFibonacciNumbers(numbers)));
+        }
+        static int[] EvenNumbers(int[] arr)
+        {
+            return arr.Where(x => x % 2 == 0).ToArray();
+        }
+        static int[] OddNumbers(int[] arr)
+        {
+            return arr.Where(x => x % 2 != 0).ToArray();
+        }
+        static int[] PrimeNumbers(int[] arr)
+        {
+            return arr.Where(IsPrime).ToArray();
+        }
+        static int[] FibonacciNumbers(int[] arr)
+        {
+            return arr.Where(IsFibonacci).ToArray();
+        }
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+        static bool IsFibonacci(int number)
+        {
+            if (number < 0) return false;
+            int a = 0, b = 1, c = 0;
+            while (c < number)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c == number;
+        }
     }
 }
